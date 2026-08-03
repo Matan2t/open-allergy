c889b3caaa44e325c62c135169bbb57a51e4787d
Matan Touti <matan.touti@mobileye.com>
2026-08-03 22:37:00 +0300

    Improve footer + buy me a coffe bottom

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -1,6 +1,11 @@
 /** Public URL of the GitHub repository (used for "edit this translation" links). */
 export const GITHUB_URL = 'https://github.com/Matan2t/open-allergy';
 
+/** Ko-fi support page (Buy Me a Coffee). */
+export const KOFI_URL = 'https://ko-fi.com/H8U524F275';
+
+export const KOFI_IMAGE = 'https://storage.ko-fi.com/cdn/kofi2.png?v=6';
+
 export const SITE_NAME = 'Open Allergy Cards';
 
 export const SITE_TAGLINE = 'Free allergy translation cards, forever.';
